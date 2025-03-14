6bd33815fee900fb05ba4313d2659aecba293f39
Ilfat Mindubaev <yagarantuz@gmail.com>
2025-03-15 00:32:36 +0200

    minor cleanup

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, useStore } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import type { AppDispatch, AppStore, RootState } from '../store/configureStore';
+import type { AppDispatch, RootState } from '../store/configureStore';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
